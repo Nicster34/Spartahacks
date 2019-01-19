a97c18e1878f813505e3f2d718a839891a36b557
nic <nic@DESKTOP-JPRCR8J>
2019-01-19 01:46:04 -0500

    Added appengine support

--- src/main/java/com/nicster34/Kalamity/KalamityApplication.java
+++ src/main/java/com/nicster34/Kalamity/KalamityApplication.java
@@ -2,6 +2,7 @@ package com.nicster34.Kalamity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class KalamityApplication {
@@ -9,6 +10,10 @@ public class KalamityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KalamityApplication.class, args);
 	}
+	  @GetMapping("/")
+	  public String hello() {
+	    return "hello world!";
+	  }
 
 }
 
